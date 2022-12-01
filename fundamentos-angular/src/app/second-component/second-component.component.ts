ad7c7ec40bb218c9f9ec0afa65b62cdc6832bd94
Mara Rúbia Alves de Oliveira <40505976+mararbia@users.noreply.github.com>
2022-12-01 11:43:10 -0300

    Text interpolation

---
+++ fundamentos-angular/src/app/second-component/second-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-second-component',
+  templateUrl: './second-component.component.html',
+  styleUrls: ['./second-component.component.css']
+})
+export class SecondComponentComponent {
+  nome = "Mara";
+  birth_date = "1986-09-06";
+}
