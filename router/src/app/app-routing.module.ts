5b0745702f9815926e06950a37d57d8c2286dbfe
Mara Rúbia Alves de Oliveira <40505976+mararbia@users.noreply.github.com>
2022-12-03 12:16:41 -0300

    Add - Initial Page and Page Not Found

---
+++ router/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { FirstPageComponent } from './first-page/first-page.component';
+import { SecondPageComponent } from './second-page/second-page.component';
+
+const routes: Routes = [
+  { path: "first-page", component: FirstPageComponent },
+  { path: "second-page", component: SecondPageComponent},
+  { path: "", redirectTo: "first-page", pathMatch: "full"},
+  { path: "**", component: PageNotFoundComponent}
+
+]
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- router/src/app/app.module.ts
+++ router/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { SecondPageComponent } from './second-page/second-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SecondPageComponent,
+    PageNotFoundComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
